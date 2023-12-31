2d3393d9beb3be8bd3f8715798a2574114251d53
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-12-31 14:25:04 -0600

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import BuggyComponent from "./BuggyComponent"
+import BuggyComponent from "./BuggyComponent";
 
 function App() {
-  
   return (
-    <div className="bg-sky-50 h-full">
-      <h1 className="text-5xl text-center text-sky-500/60">Title Goes Here</h1>
-      <BuggyComponent className="text-center flex flex-col justify-center items-center mt-4"/>
+    <div className="h-full bg-sky-50">
+      <h1 className="text-center text-5xl text-sky-500/60">
+        Lorem Ipsum Dolor
+      </h1>
+      <BuggyComponent className="mx-auto mt-4 flex w-[75%] flex-col items-center justify-center text-center">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+        quibusdam, porro sequi odio deserunt, optio sit hic quod ex quia fugiat
+        aut placeat iusto iste aspernatur ipsum sunt debitis ab alias molestiae
+        ad tempora deleniti atque? Est repellat molestias fugiat, aliquid sint
+        ullam minus laudantium corporis ipsam autem voluptate quo officia cumque
+        rerum iste expedita odit delectus non nobis blanditiis natus quia at
+        necessitatibus. Ex molestias animi nemo laborum, labore adipisci ab
+        corporis odio sit et cupiditate numquam omnis veritatis dignissimos
+        repellendus nam amet consequuntur, quod minima. Ea eos dolor doloremque
+        reiciendis quisquam! Voluptatibus facere veniam modi unde aspernatur ea.
+      </BuggyComponent>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/BuggyComponent.tsx
+++ src/BuggyComponent.tsx
@@ -1,16 +1,21 @@
-import success from './assets/success.jpeg'
+import success from "./assets/success.jpeg";
 
 interface myProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
-    throwError?: boolean
+  throwError?: boolean;
 }
-export default function BuggyComponent(props: myProps) {
-    if (props.throwError) {
-        throw new Error('Ohh No there was an error!')
-    }
-    return (
-        <div className={props.className}>
-            <h2 className='text-4xl text-sky-400/80 mt-4'>Success!!</h2>
-            <img src={success} alt="Success" width={400} />
-        </div>
-    )
+export default function BuggyComponent({
+  className,
+  throwError,
+  ...props
+}: myProps) {
+  if (throwError) {
+    throw new Error("Ohh No there was an error!");
+  }
+  return (
+    <div className={className}>
+      <h2 className="text-4xl text-sky-400/80">Success!!</h2>
+      <img src={success} alt="Success" width={400} />
+      <div className="p-4 indent-8">{props.children}</div>
+    </div>
+  );
 }
